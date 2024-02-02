55125f4c3906820cf067947a54830696825ae1a1
Robert Furuvald <rfu@kth.se>
2024-02-02 14:54:23 +0100

    test crossOrigin with react app

--- src/main/java/com/server/backend/controller/AuthController.java
+++ src/main/java/com/server/backend/controller/AuthController.java
@@ -13,12 +13,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") //TODO: for testing now and then when frontend is deployed to heroku change the origins
 @RequestMapping("/api/auth")
 public class AuthController {
 
