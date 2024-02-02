851a385915f282e78d76fceead86592d35878faf
Robert Furuvald <rfu@kth.se>
2024-02-02 15:50:49 +0100

    test to add CORS config

--- src/main/java/com/server/backend/config/WebConfig.java
+++ src/main/java/com/server/backend/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.server.backend.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@Configuration
 public class WebConfig {
 
     @Bean
