da919c2a37d64342f7724fb6549096277f0b733a
Robert Furuvald <rfu@kth.se>
2024-02-05 17:10:38 +0100

    adding heroku cors settings

--- src/main/java/com/server/backend/config/WebConfig.java
+++ src/main/java/com/server/backend/config/WebConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,13 +16,14 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://kth-recruitment-frontend-3b6226a80ec7.herokuapp.com/")
                         .allowedMethods(HttpMethod.GET.name(),
                                 HttpMethod.POST.name()).allowedHeaders(HttpHeaders.CONTENT_TYPE,
                                 HttpHeaders.AUTHORIZATION);
             }
         };
     }
+}
 /*    public CorsConfigurationSource corsConfiguration() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.applyPermitDefaultValues();
@@ -34,4 +32,3 @@ public class WebConfig {
         source.registerCorsConfiguration("/**", corsConfig);
         return source;
     }*/
-}
