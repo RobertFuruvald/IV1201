f2ceb5eca105189988e9e6da7fb3e1cc917b7829
elkhjean <sean.elkhoury@gmail.com>
2024-02-07 00:01:12 +0100

    javadocumentation

--- src/main/java/com/server/backend/dto/AuthenticationRequest.java
+++ src/main/java/com/server/backend/dto/AuthenticationRequest.java
@@ -1,8 +1,16 @@
 package com.server.backend.dto;
 
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+
+/**
+ * DTO for carrying authentication request details.
+ * This class is used to encapsulate the user credentials provided during login requests.
+ * <p>
+ * It includes the username and password fields, which are required for authentication.
+ * The class is annotated with Lombok's {@code @Getter} to automatically generate getters,
+ */
+@Getter
 public class AuthenticationRequest {
     private String username;
     private String password;
--- src/main/java/com/server/backend/dto/RegistrationDTO.java
+++ src/main/java/com/server/backend/dto/RegistrationDTO.java
@@ -2,6 +2,13 @@ package com.server.backend.dto;
 
 import lombok.Getter;
 
+/**
+ * DTO for carrying user registration details.
+ * This class encapsulates the information provided by a user during the registration process.
+ * <p>
+ * It includes name, surname, personal number (pnr), email, username and password.
+ * The class is annotated with Lombok's {@code @Getter} to automatically generate getters,
+ */
 @Getter
 public class RegistrationDTO {
 
