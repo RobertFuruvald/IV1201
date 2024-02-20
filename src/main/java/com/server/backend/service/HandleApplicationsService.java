3d6ec95f2f04b95feec925b79cb3616052649140
elkhjean <sean.elkhoury@gmail.com>
2024-02-20 18:37:18 +0100

    transaction safe

--- src/main/java/com/server/backend/service/HandleApplicationsService.java
+++ src/main/java/com/server/backend/service/HandleApplicationsService.java
@@ -11,12 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@Transactional
 @Service
 public class HandleApplicationsService {
 
--- src/main/java/com/server/backend/service/PersonService.java
+++ src/main/java/com/server/backend/service/PersonService.java
@@ -3,6 +3,8 @@ package com.server.backend.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.server.backend.entity.Person;
 import com.server.backend.dto.PersonUpdateDTO;
 import com.server.backend.repository.PersonRepository;
@@ -12,6 +14,7 @@ import java.util.Optional;
 /**
  * Service class for updating person information.
  */
+@Transactional
 @Service
 public class PersonService {
 
